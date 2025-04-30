8298d280bacff2d361826eee63b73e037ca6d2d3
Gregg Emperado <greggmemperado@su.edu.ph>
2025-04-30 13:58:29 +0800

    added Bootstrap 5

---
+++ CS36 Project/AdminRoomManagement.php
@@ -0,0 +1,32 @@
+<?php
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $maindb = "emperado dbe6";
+
+
+// $conn = new mysqli($servername, $username, $password);
+
+
+// if($conn->connect_error){
+//    die("Connection failed: ". $conn->connect_error);
+// }
+?>
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="Hotel.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+    <div class="container">
+  <h1>My First Bootstrap Page</h1>
+  <p>This part is inside a .container class.</p>
+  <p>The .container class provides a responsive fixed width container.</p>
+</div>
+        <header>
+            <!-- <img src = "resources\RKGlogo.png"> -->
+        </header>
+    
+    </body>
+</html>
